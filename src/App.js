5b726ceee0290514ace76e371dbe66f572b70087
UnicusStella <unicusstella@gmail.com>
2021-08-20 18:16:58 +0900

    Router Path Complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // TODO - react-router-dom을 설치 후, import 구문을 이용하여 BrowserRouter, Route, Switch 컴포넌트를 불러오세요.
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Sidebar from './Sidebar';
 import Tweets from './Pages/Tweets';
 // TODO - import문을 이용하여 MyPage, About 컴포넌트를 불러오세요.
+import About from './Pages/About';
+import MyPage from './Pages/MyPage';
 
 const App = () => {
   return (
@@ -18,8 +20,14 @@ const App = () => {
             <section className="features">
               {/* TODO - Switch와 Route 컴포넌트를 이용하여 경로(path)를 설정하고 Tweets, Mypage, About 컴포넌트를 연결합니다. */}
               <Switch>
-                <Route>
+                <Route exact path="/">
                   <Tweets />
+                </Route>
+                <Route path="/about">
+                  <About />
+                </Route>
+                <Route path="/mypage">
+                  <MyPage />
                 </Route>
               </Switch>
             </section>
