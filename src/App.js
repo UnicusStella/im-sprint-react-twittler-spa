7b9a07a66d3b8fd50ba9369cc6a6f1af430777ed
UnicusStella <unicusstella@gmail.com>
2021-08-20 18:12:54 +0900

    React Router Complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // TODO - react-router-dom을 설치 후, import 구문을 이용하여 BrowserRouter, Route, Switch 컴포넌트를 불러오세요.
 
 import Sidebar from './Sidebar';
@@ -10,15 +11,21 @@ const App = () => {
   return (
     <div>
       {/* TODO - BrowserRouter 컴포넌트를 작성합니다. */}
+      <BrowserRouter>
         <div className="App">
           <main>
             <Sidebar />
             <section className="features">
               {/* TODO - Switch와 Route 컴포넌트를 이용하여 경로(path)를 설정하고 Tweets, Mypage, About 컴포넌트를 연결합니다. */}
-              <Tweets />
+              <Switch>
+                <Route>
+                  <Tweets />
+                </Route>
+              </Switch>
             </section>
           </main>
         </div>
+      </BrowserRouter>
     </div>
   );
 };
